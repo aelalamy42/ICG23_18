1cea279fcadd1f7124f482ffba1530cc9f943db2
Ahmed Elalamy <ahmedkamalelalamy@gmail.com>
2023-03-16 14:00:01 +0100

    Task 1 done

--- GL1_transforms/src/main_2d_transform.js
+++ GL1_transforms/src/main_2d_transform.js
@@ -79,7 +79,7 @@ async function main() {
 		void main() {
 			// #TODO GL1.1.1.1 Edit the vertex shader to apply mouse_offset translation to the vertex position.
 			// We have to return a vec4, because homogenous coordinates are being used.
-			gl_Position = vec4(position, 0, 1);
+			gl_Position = vec4(position + mouse_offset, 0, 1);
 		}`,
 			
 		/* 
@@ -122,7 +122,7 @@ async function main() {
 
 		void main() {
 			// #TODO GL1.1.2.1 Edit the vertex shader to apply mat_transform to the vertex position.
-			gl_Position = vec4(position, 0, 1);
+			gl_Position = mat_transform * vec4(position, 0, 1);
 		}`,
 		
 		frag: /*glsl*/`
@@ -192,8 +192,8 @@ async function main() {
 		// #TODO GL1.1.1.2 Draw the blue triangle translated by mouse_offset
 		
 		draw_triangle_with_offset({
-			mouse_offset: [0, 0],
-			color: [0.5, 0.5, 0.5],
+			mouse_offset: mouse_offset,
+			color: color_blue,
 		});
 
 		/*
@@ -205,15 +205,22 @@ async function main() {
 				* a red triangle spinning at [0.5, 0, 0]
 			You do not have to apply the mouse_offset to them.
 		*/
-		//draw_triangle_with_transform({
-		//	mat_transform: mat_transform,
-		//	color: [0.5, 0.5, 0.5],
-		//});
+		const rotation = mat4.fromZRotation(mat4.create(), sim_time * Math.PI / 6.); 
+		const translation = mat4.fromTranslation(mat4.create(), [0.5, 0., 0.]);
+		const rotateThenTranslate = mat4.multiply(mat4.create(), translation, rotation);
+		const translateThenRotate = mat4.multiply(mat4.create(), rotation, translation);
 
-		//draw_triangle_with_transform({
-		//	mat_transform: mat_transform,
-		//	color: [0.5, 0.5, 0.5],
-		//});
+		console.log(mat4_to_string(translateThenRotate));
+
+		draw_triangle_with_transform({
+			mat_transform: translateThenRotate,
+			color: color_green,
+		});
+
+		draw_triangle_with_transform({
+			mat_transform: rotateThenTranslate,
+			color: color_red,
+		});
 
 		// You can write whatever you need in the debug box
 		debug_text.textContent = `
